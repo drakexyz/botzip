7201d9aba2cd08fcbe825b81d0cd4a92f594596c
nomeualquer <drakeahkk@gmail.com>
2022-03-17 23:39:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+__Você precisa criar um bot no [@botfather](https://t.me/botfather) para fazer a instalação__
+
+Instalação simples, apenas execute esse codigo a sua vps para que o seu bot começe a funcionar, durante a instalação será preciso fornescer algumas informações que o bot ira precisar.
+
+```wget https://raw.githubusercontent.com/developper777/botsshfree/master/iniciar.sh -O iniciar.sh; chmod +x iniciar.sh; ./iniciar.sh```
+
+
+Com esse script você terá um bot de contas SSH gratís e funçional.
+
+__Pacotes nescessarios:__
+- php
+- php-curl
+- php-redis
+- redis-server
+- screen
+- zip
+
+nosso telegram 
+https://t.me/EQUIPEVIPSUPORTE
+
---
+++ bot.php
@@ -0,0 +1,109 @@
+<?php
+
+// Bot criado por @drakekk1 código original para o @drakekk1
+
+date_default_timezone_set ('America/Sao_Paulo'); // define timestamp padrão
+
+// Incluindo arquivos nescessários
+include __DIR__.'/Telegram.php';
+
+if (!file_exists('dadosBot.ini')){
+
+	echo "Faça a instalação do bot antes!";
+	exit;
+
+}
+
+$textoMsg=json_decode (file_get_contents('textos.json'));
+$iniParse=parse_ini_file('dadosBot.ini');
+
+$ip=$iniParse ['ip'];
+$token=$iniParse ['token'];
+$limite=$iniParse ['limite'];
+
+define ('5059987771:AAHjZHJ8do_dZqo2_-JJwIoWZIen0OW6Rmk', $token); // token do bot criado no @botfather
+
+// Instancia das classes
+$tlg=new Telegram (TOKEN);
+$redis=new Redis ();
+$redis->connect ('localhost', 6379); //redis usando porta padrão
+
+// BLOCO USADO EM LONG POLLING
+
+while (true){
+
+$updates=$tlg->getUpdates();
+
+for ($i=0; $i < $tlg->UpdateCount(); $i++){
+
+$tlg->serveUpdate($i);
+
+switch ($tlg->Text ()){
+
+	case '/start':
+
+	$tlg->sendMessage ([
+		'chat_id' => $tlg->ChatID (),
+		'text' => $textoMsg->start,
+		'parse_mode' => 'html',
+		'reply_markup' => $tlg->buildInlineKeyBoard ([
+			[$tlg->buildInlineKeyboardButton ('🇧🇷 SSH Gratis BR 🇧🇷', null, '/sshgratis')]
+		])
+	]);
+
+	break;
+	case '/sobre':
+
+	$tlg->sendMessage ([
+		'chat_id' => $tlg->ChatID (),
+		'text' => 'DONO @DRAKEKK1 por @drakekk1'
+	]);
+
+	break;
+	case '/total':
+
+	$tlg->sendMessage ([
+		'chat_id' => $tlg->ChatID (),
+		'text' => 'Foram criadas <b>'.$redis->dbSize ().'</b> contas nas ultimas 24h',
+		'parse_mode' => 'html'
+	]);
+
+	break;
+	case '/sshgratis':
+
+	$tlg->answerCallbackQuery ([
+	'callback_query_id' => $tlg->Callback_ID()
+	]);
+
+	if ($redis->dbSize () == $limite){
+
+		$textoSSH=$textoMsg->sshgratis->limite;
+
+	} elseif ($redis->exists ($tlg->UserID ())){
+
+		$textoSSH=$textoMsg->sshgratis->nao_criado;
+
+	} else {
+
+		$usuario=substr (str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 6);
+		$senha=mt_rand(11111, 999999);
+
+		exec ('./gerarusuario.sh '.$usuario.' '.$senha.' 1 1');
+
+		$textoSSH="🇧🇷 Conta SSH criada ;)\r\n\r\n<b>Servidor:</b> <code>".$ip."</code>\r\n<b>Usuario:</b> <code>".$usuario."</code>\r\n<b>Senha:</b> <code>".$senha."</code>\r\n<b>Logins:</b> 1\r\n<b>Validade:</b> ".date ('d/m', strtotime('+1 day'))."\r\n\r\n🤙 Cortesia do @drakekk1";
+
+		$redis->setex ($tlg->UserID (), 43200, 'true'); //define registro para ser guardado por 12h
+
+	}
+
+	$tlg->sendMessage ([
+		'chat_id' => $tlg->ChatID (),
+		'text' => $textoSSH,
+		'parse_mode' => 'html'
+	]);
+
+	break;
+
+}
+
+}}
